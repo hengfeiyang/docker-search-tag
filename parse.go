443961b1e98e7eb3f70fe08c40e537f5d7ee55da
Coldstar <safeie@163.com>
2021-12-14 17:59:20 +0800

    format output

--- parse.go
+++ parse.go
@@ -15,19 +15,24 @@ func parse(name string, respBody []byte) {
 		return
 	}
 	if len(respData.Results) == 0 {
-		fmt.Printf("there is no tag data for name: %s", name)
+		fmt.Printf("there is no tag data for name: %s\n", name)
 		return
 	}
 
 	// resort results
 	sort.Sort(respData.Results)
 
-	fmt.Printf("Tags for %s:\n", name)
-	fmt.Println("-------------------------------")
-	fmt.Printf("%s %16s %s\n", "Tag", "", "Size(MB)")
-	fmt.Println("--------------------------------")
+	maxLengthForName := 10
 	for _, v := range respData.Results {
-		fmt.Printf("%s%s %d\n", v.Name, strings.Repeat(" ", max(0, 20-len(v.Name))), v.FullSize/1024/1024)
+		if len(v.Name) > maxLengthForName {
+			maxLengthForName = len(v.Name)
+		}
+	}
+
+	fmt.Printf("Tags for %s:\n\n", name)
+	fmt.Printf("%s%s%s\n", "TAG", strings.Repeat(" ", maxLengthForName), "SIZE(MB)")
+	for _, v := range respData.Results {
+		fmt.Printf("%s%s   %d\n", v.Name, strings.Repeat(" ", max(0, maxLengthForName-len(v.Name))), v.FullSize/1024/1024)
 	}
 }
 
