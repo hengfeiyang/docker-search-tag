741b445b22278bde55580977fe53e8e2c0ba2b51
Coldstar <safeie@163.com>
2021-12-16 18:16:58 +0800

    add tag flag for search tags

--- main.go
+++ main.go
@@ -4,42 +4,24 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"os"
 )
 
 func main() {
-	var name, num string
+	var name, tag, num string
 	if len(os.Args) < 2 || len(os.Args[1]) == 0 {
-		fmt.Println("Usage: docker-search-tag NAME [NUM]")
+		fmt.Println("Usage: docker-search-tag NAME [TAG] [NUM]")
 		return
 	}
 
 	name = os.Args[1]
 	num = "100"
-	if len(os.Args) == 3 && len(os.Args[2]) > 0 {
-		num = os.Args[2]
+	if len(os.Args) >= 3 && len(os.Args[2]) > 0 {
+		tag = os.Args[2]
+	}
+	if len(os.Args) == 4 && len(os.Args[3]) > 0 {
+		num = os.Args[3]
 	}
 
-	request(name, num)
-}
-
-func request(name, num string) {
-	url := fmt.Sprintf("https://registry.hub.docker.com/v2/repositories/library/%s/tags/?page_size=%s", name, num)
-	resp, err := http.Get(url)
-	if err != nil {
-		fmt.Printf("Get information from registry.hub.docker.com err: %v\n", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	respBody, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Printf("Read data err: %v\n", err)
-		return
-	}
-
-	// fmt.Printf("data json:\n%s\n", respBody)
-	parse(name, respBody)
+	request(name, tag, num)
 }
